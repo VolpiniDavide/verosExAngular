5ca25d5e6701d0d594927e9e18413f28581f9ebb
davide <volpini.davide1@gmail.com>
2018-12-04 17:22:06 +0100

    lista ricette funzionante

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
 
-  currentRicetta: Recipe;
+ @Input() currentRicetta: Recipe;
 
   constructor() { }
 
--- src/app/recipes/recipe-list/recipe.model.ts
+++ src/app/recipes/recipe-list/recipe.model.ts
@@ -11,4 +11,5 @@ export class Recipe {
       this.imagePath = imgP;
   }
 
+
 }
