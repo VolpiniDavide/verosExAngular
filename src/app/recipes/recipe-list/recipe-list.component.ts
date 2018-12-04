929cda01ce4ffec55480dc3eb3045ce1460dd781
davide <volpini.davide1@gmail.com>
2018-12-04 16:58:16 +0100

    recipe list e item
    creato oggetto ricetta (  Recipe ).
    creato array di oggetti Recipe chiamato recipes.
    ciclato array recipes.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe [] = [
+
+    new Recipe(
+      "Patate riso e cozze",
+      " una buona ricetta",
+      "https://ips.plug.it/cips/buonissimo.org/cms/2012/04/tiella-di-riso-patate-e-cozze.jpg"
+      ),
+
+    new Recipe(
+      "pizza",
+      " la più buona di tutte",
+      "https://img.grouponcdn.com/deal/8DDtq5XRzVnLXEUnPHPd/p2-2048x1229/v1/c700x420.jpg"
+    ),
+
+    new Recipe(
+      "bistecca",
+      " al sangue è meglio",
+      "https://carnedistruzzo.it/wp-content/uploads/carne-bistecca-163614.jpg"
+      ),
+
+    new Recipe(
+      "torta",
+      " diabaete prendimi",
+      "https://images.lacucinaitaliana.it/wp-content/uploads/2016/09/Torta-di-compleanno.jpg"
+    ),
+    ];
 
   constructor() { }
 
